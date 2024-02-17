5a13e3ee149a39300eb03ab979ec96b63ef44519
Yaya Mamoudou <57509871+yaya-mamoudou@users.noreply.github.com>
2024-02-17 19:21:43 +0100

    crawl page recursively

--- crawl.js
+++ crawl.js
@@ -4,22 +4,22 @@ export function normalizeUrl(url) {
     const urlObject = new URL(url)
     const hostPath = `${urlObject.hostname}${urlObject.pathname}`
 
-    if (hostPath.length > 0 && hostPath.slice(-1) === '/') {
+    if (hostPath.length > 0 && hostPath.trim().slice(-1) === '/') {
         return hostPath.slice(0, -1)
     }
 
     return hostPath
 }
-
+''.trim()
 export function getUrlFromHTML(htmlBody, baseUrl) {
-    if (baseUrl.endsWith('/')) baseUrl = baseUrl.slice(0, -1)
+    if (baseUrl.trim().endsWith('/')) baseUrl = baseUrl.slice(0, -1)
 
     const dom = new JSDOM(htmlBody)
     const linkElements = dom.window.document.querySelectorAll('a')
     const urls = []
 
     linkElements.forEach(elementURL => {
-        if (elementURL.href[0] === '/') {
+        if (elementURL.href.trim()[0] === '/') {
             elementURL.href = `${baseUrl}${elementURL.href}`
             urls.push(elementURL.href)
         }
@@ -37,11 +37,24 @@ export function getUrlFromHTML(htmlBody, baseUrl) {
     return urls
 }
 
-export async function crawlPage(currentUrl) {
-    console.log(`Crawling: ${currentUrl}`)
+export async function crawlPage(baseUrl, currentURL, pages) {
+
+    const baseURLObj = new URL(baseUrl)
+    const currentURLObj = new URL(currentURL)
+    if (baseURLObj.hostname !== currentURLObj.hostname) return pages
+
+    const normalizedCurrentURL = normalizeUrl(currentURL)
+    if (pages[normalizedCurrentURL] > 0) {
+        pages[normalizedCurrentURL]++
+        return pages
+    }
+
+    pages[normalizedCurrentURL] = 1
+
+    console.log(`Actively crawling: ${currentURL}`)
 
     try {
-        const res = await fetch(currentUrl);
+        const res = await fetch(currentURL)
         const contentType = res.headers.get('content-type')
 
         if (!contentType.includes('text/html')) {
@@ -50,9 +63,18 @@ export async function crawlPage(currentUrl) {
 
         if (res.status > 399) throw new Error(`status code - ${res.status}`)
 
-        return await res.text()
+        const htmlData = await res.text()
+        const nextURLs = getUrlFromHTML(htmlData, baseUrl)
+
+        for (const nextURL of nextURLs) {
+            pages = await crawlPage(baseUrl, nextURL, pages)
+        }
+
     } catch (error) {
-        console.error(`\nError in fetching: ${error.message}\nPage: ${currentUrl}
-        `);
+        console.error(`\nError in fetching: ${error.message}\nPage: ${currentURL}`)
+    }
+
+    finally {
+        return pages
     }
 }
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import { crawlPage, getUrlFromHTML } from './crawl.js'
+import { crawlPage } from './crawl.js'
 
-function main() {
+async function main() {
     if (process.argv.length < 3) {
         console.log("no website provided")
         process.exit(1)
@@ -13,10 +13,8 @@ function main() {
     const baseURL = process.argv[2]
     console.log(`starting crawl of ${baseURL}`)
 
-    crawlPage(baseURL).then(page => {
-        const urls = getUrlFromHTML(page, baseURL)
-        console.log(urls);
-    })
+    const pages = await crawlPage(baseURL, baseURL, {})
+    console.log(pages, '====================');
 }
 
 main()
